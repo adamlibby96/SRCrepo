9b6de145713845ded5e750c1e1c9331df67e053a
adamlibby96 <adamlibby96@gmail.com>
2017-02-28 01:29:26 -0600

    Second Commit
    Testing to see if the repo is working properly

--- SimpleRiscComputer/src/srcGUI/MemoryPanel.java
+++ SimpleRiscComputer/src/srcGUI/MemoryPanel.java
@@ -9,7 +9,8 @@ import javax.swing.JTextArea;
 
 public class MemoryPanel extends JPanel
 {
-	
+	// testing to see if the Repo is working properly!!
+
 	/**
 	 * Create the panel.
 	 */
@@ -17,26 +18,26 @@ public class MemoryPanel extends JPanel
 	{
 		this.setBounds(100, 100, 1920, 1080);
 		setLayout(null);
-
+		
 		JTextArea instructionTA = new JTextArea();
 		instructionTA.setBounds(991, 52, 911, 980);
 		add(instructionTA);
-
+		
 		JLabel lblEnterTheProgram = new JLabel("Enter the program instructions. (END EACH INSTRUCTION WITH A ';')");
 		lblEnterTheProgram.setBounds(991, 13, 483, 26);
 		add(lblEnterTheProgram);
-
+		
 		JButton btnExecute = new JButton("EXECUTE");
 		btnExecute.setBounds(1805, 14, 97, 25);
 		add(btnExecute);
-
+		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(1358, 216, 2, 2);
 		instructionTA.add(scrollPane);
-
+		
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(0, 52, 990, 980);
 		add(scrollPane_1);
-		
+
 	}
 }
