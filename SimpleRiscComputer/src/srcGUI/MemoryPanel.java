cf0ad8fb10159247d72bfdb27642355d8d0fd1e1
adamlibby96 <adamlibby96@gmail.com>
2017-03-02 10:41:00 -0600

    Not sure what change i made here.

--- SimpleRiscComputer/src/srcGUI/MemoryPanel.java
+++ SimpleRiscComputer/src/srcGUI/MemoryPanel.java
@@ -12,44 +12,44 @@ import javax.swing.JTextArea;
 import computer.Memory;
 import problemDomain.Computer;
 
-public class MemoryPanel extends JPanel
+public class memoryPanel extends JPanel
 {
 	JList<Memory> list;
 	DefaultListModel<Memory> listModel;
-	
+
 	/**
 	 * Create the panel.
 	 */
-	public MemoryPanel(JFrame frame, Computer pc)
+	public memoryPanel(JFrame frame, Computer pc)
 	{
 		this.setBounds(100, 100, 1920, 1080);
 		setLayout(null);
-
+		
 		JTextArea instructionTA = new JTextArea();
 		instructionTA.setBounds(991, 52, 911, 980);
 		add(instructionTA);
-
+		
 		JLabel lblEnterTheProgram = new JLabel("Enter the program instructions. (END EACH INSTRUCTION WITH A ';')");
 		lblEnterTheProgram.setBounds(991, 13, 483, 26);
 		add(lblEnterTheProgram);
-
+		
 		JButton btnExecute = new JButton("EXECUTE");
 		btnExecute.setBounds(1805, 14, 97, 25);
 		add(btnExecute);
-
+		
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(1358, 216, 2, 2);
 		instructionTA.add(scrollPane);
-		
-		listModel = new DefaultListModel<Memory>();
-		
-		list = new JList<Memory>(listModel);
 
+		listModel = new DefaultListModel<Memory>();
+
+		list = new JList<Memory>(listModel);
+		
 		JScrollPane scrollPane_1 = new JScrollPane();
 		scrollPane_1.setBounds(0, 52, 990, 980);
 		add(scrollPane_1);
-		
-		
-		
+
+
+
 	}
 }
